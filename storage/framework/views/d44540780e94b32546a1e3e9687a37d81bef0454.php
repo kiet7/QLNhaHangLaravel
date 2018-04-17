d42219fe90d65441cb5971c0582d81a27ea6b13e
Phan Trong <phantoms15897@gmail.com>
2018-04-17 19:13:28 +0700

    second commit

---
+++ storage/framework/views/d44540780e94b32546a1e3e9687a37d81bef0454.php
@@ -0,0 +1,7 @@
+<?php $__env->startSection('socket'); ?>
+
+
+    
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout.masteradmin', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
---
+++ storage/framework/views/f9472543139060c8f8b8a87be50b8cc60562de46.php
@@ -0,0 +1,83 @@
+<?php $__env->startSection('content'); ?>
+        <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Data Tables
+        <small>advanced tables</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Tables</a></li>
+        <li class="active">Data tables</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-sm-3">
+          <div class="card" style="margin-top: 2rem;">
+            <img class="card-img-top" src="http://via.placeholder.com/150x150" alt="Card image cap">
+            <div class="card-block">
+              <h4 class="card-title">Card title</h4>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Button</a>
+            </div>
+
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="card" style="margin-top: 2rem;">
+            <img class="card-img-top" src="http://via.placeholder.com/150x150" alt="Card image cap">
+            <div class="card-block">
+              <h4 class="card-title">Card title</h4>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Button</a>
+            </div>
+            
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="card" style="margin-top: 2rem;">
+            <img class="card-img-top" src="http://via.placeholder.com/150x150" alt="Card image cap">
+            <div class="card-block">
+              <h4 class="card-title">Card title</h4>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Button</a>
+            </div>
+            
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="card" style="margin-top: 2rem;">
+            <img class="card-img-top" src="http://via.placeholder.com/150x150" alt="Card image cap">
+            <div class="card-block">
+              <h4 class="card-title">Card title</h4>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Button</a>
+            </div>
+            
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="card" style="margin-top: 2rem;">
+            <img class="card-img-top" src="http://via.placeholder.com/150x150" alt="Card image cap">
+            <div class="card-block">
+              <h4 class="card-title">Card title</h4>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Button</a>
+            </div>
+            
+          </div>
+        </div>
+          
+    </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('../layout.masteradmin', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
